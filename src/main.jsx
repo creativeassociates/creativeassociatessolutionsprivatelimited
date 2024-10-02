bf612b8480be05c490756c087f78b617db48e93a
StagerCoder <rohankakkar1997@gmail.com>
2024-10-03 01:11:51 +0530

    Changed HashRouter To BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import {HashRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import {ThemeProvider} from "@material-tailwind/react";
 import "./css/tailwind.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <HashRouter>
+    <BrowserRouter basename={"/"}>
         <ThemeProvider>
             <App/>
         </ThemeProvider>
-    </HashRouter>
+    </BrowserRouter>
 );
